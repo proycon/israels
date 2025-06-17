d8c63e0e0a7e4ce0856dafde1d7a5ca5f9a58cde
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2025-06-17 17:40:10 +0200

    modified surfdrive folder location

--- programs/syncsurfdrive.sh
+++ programs/syncsurfdrive.sh
@@ -2,21 +2,21 @@
 
 cd ..
 printf "start thumb"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' thumb surfdrive:Israels-Scans-Curated/thumb
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' thumb surfdrive:Shared/Israels-Scans-Curated/thumb
 printf "end   thumb"
 
 printf "start scans"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' scans surfdrive:Israels-Scans-Curated/scans
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' scans surfdrive:Shared/Israels-Scans-Curated/scans
 printf "end   scans"
 
 printf "start original-backsides"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansBacksides surfdrive:Israels-Scans-Curated/original-backsides
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansBacksides surfdrive:Shared/Israels-Scans-Curated/original-backsides
 printf "end   original-backsides"
 
 printf "start original-revisited"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrigRevisited surfdrive:Israels-Scans-Curated/original-revisited
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrigRevisited surfdrive:Shared/Israels-Scans-Curated/original-revisited
 printf "end   original-revisited"
 
 # printf "start original"
-# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrig surfdrive:Israels-Scans-Curated/original
+# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrig surfdrive:Shared/Israels-Scans-Curated/original
 # printf "end   original"
