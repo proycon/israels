395c6bd4befabfd5c43cfa7c52ad1c82350301ab
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2025-06-17 16:33:26 +0200

    processed additional scans

--- programs/syncsurfdrive.sh
+++ programs/syncsurfdrive.sh
@@ -1,7 +1,22 @@
 #!/bin/zsh
 
 cd ..
-# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' scans surfdrive:Israels-Scans-Curated/jpg
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrig surfdrive:Israels-Scans-Curated/original
+printf "start thumb"
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' thumb surfdrive:Israels-Scans-Curated/thumb
+printf "end   thumb"
+
+printf "start scans"
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' scans surfdrive:Israels-Scans-Curated/scans
+printf "end   scans"
+
+printf "start original-backsides"
+rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansBacksides surfdrive:Israels-Scans-Curated/original-backsides
+printf "end   original-backsides"
+
+printf "start original-revisited"
 rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrigRevisited surfdrive:Israels-Scans-Curated/original-revisited
-# rclone -v sync --no-update-modtime  --delete-excluded --exclude '.DS_Store' curatedscans surfdrive:Translatin-Sources-Scans
+printf "end   original-revisited"
+
+# printf "start original"
+# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrig surfdrive:Israels-Scans-Curated/original
+# printf "end   original"
