72f2802fa0a0369de28c771e58116fef43d30422
Dirk Roorda <dirk.roorda@di.huc.knaw.nl>
2025-09-01 11:09:09 +0200

    3 updated images

--- programs/syncsurfdrive.sh
+++ programs/syncsurfdrive.sh
@@ -9,13 +9,13 @@ printf "start scans"
 rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' scans surfdrive:Shared/Israels-Scans-Curated/scans
 printf "end   scans"
 
-printf "start original-backsides"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansBacksides surfdrive:Shared/Israels-Scans-Curated/original-backsides
-printf "end   original-backsides"
+# printf "start original-backsides"
+# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansBacksides surfdrive:Shared/Israels-Scans-Curated/original-backsides
+# printf "end   original-backsides"
 
-printf "start original-revisited"
-rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrigRevisited surfdrive:Shared/Israels-Scans-Curated/original-revisited
-printf "end   original-revisited"
+# printf "start original-revisited"
+# rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrigRevisited surfdrive:Shared/Israels-Scans-Curated/original-revisited
+# printf "end   original-revisited"
 
 # printf "start original"
 # rclone -v sync --no-update-modtime --delete-excluded --exclude '.DS_Store' _local/ScansOrig surfdrive:Shared/Israels-Scans-Curated/original
